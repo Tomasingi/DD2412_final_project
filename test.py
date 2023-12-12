ea7f6ff07449fffcf5ccf06d30056dd5c5c02457
Tómas Ingi <tomasingi00@gmail.com>
2023-12-12 15:43:55 +0100

    Add metrics to the device

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ def test_cycle(model, hparams, val_loader):
     Calculates Acc, Cross-entropy, ECE, AUPR, AUC and FPR95
     """
 
-    acc = torchmetrics.Accuracy(task='multiclass', num_classes=10)
-    ece = torchmetrics.CalibrationError(task='multiclass', num_classes=10)
-    aupr = torchmetrics.AUROC(task='multiclass', num_classes=10)
-    auc = torchmetrics.AUROC(task='multiclass', num_classes=10)
-    # fpr95 = torchmetrics.FalsePositiveRate(num_thresholds=1000, pos_label=1, compute_on_step=False)
+    acc = torchmetrics.Accuracy(task='multiclass', num_classes=10).to(hparams.device)
+    ece = torchmetrics.CalibrationError(task='multiclass', num_classes=10).to(hparams.device)
+    aupr = torchmetrics.AUROC(task='multiclass', num_classes=10).to(hparams.device)
+    auc = torchmetrics.AUROC(task='multiclass', num_classes=10).to(hparams.device)
+    # fpr95 = torchmetrics.FalsePositiveRate(num_thresholds=1000, pos_label=1, compute_on_step=False).to(hparams.device)
 
     model = model.to(hparams.device)
 
