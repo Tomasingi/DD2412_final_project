86c07741589390969a89678808feda35ee553969
Tómas Ingi <tomasingi00@gmail.com>
2023-12-14 15:13:35 +0100

    update tests

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def test_cycle(model, hparams, val_loader):
     acc = torchmetrics.Accuracy(task='multiclass', num_classes=10).to(hparams.device)
     ece = torchmetrics.CalibrationError(task='multiclass', num_classes=10).to(hparams.device)
     aupr = torchmetrics.classification.MulticlassAveragePrecision(num_classes=10).to(hparams.device)
-    auc = torchmetrics.AUROC(task='multiclass', num_classes=10).to(hparams.device)
+    auc = torchmetrics.classification.MulticlassAUROC(num_classes=10).to(hparams.device)
     # fpr95 = torchmetrics.FalsePositiveRate(num_thresholds=1000, pos_label=1, compute_on_step=False).to(hparams.device)
     model = model.to(hparams.device)
     model.eval()
@@ -26,10 +26,10 @@ def test_cycle(model, hparams, val_loader):
             auc.update(outputs, labels)
             # fpr95.update(outputs, labels)
 
-    acc = acc.compute()
-    ece = ece.compute()
-    aupr = aupr.compute()
-    auc = auc.compute()
-    # fpr95 = fpr95.compute()
+    total_acc = acc.compute()
+    total_ece = ece.compute()
+    total_aupr = aupr.compute()
+    total_auc = auc.compute()
+    # total_fpr95 = fpr95.compute()
 
-    return acc, ece, aupr, auc#, fpr95
+    return total_acc, total_ece, total_aupr, total_auc#, total_fpr95
