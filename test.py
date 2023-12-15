ab45bd262089d9b14c62c3d091c54e6cbce9a626
Tómas Ingi <tomasingi00@gmail.com>
2023-12-15 13:32:53 +0100

    softmax for tests

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def test_cycle(model, hparams, val_loader):
     ece = torchmetrics.CalibrationError(task='multiclass', num_classes=10).to(hparams.device)
     aupr = torchmetrics.classification.MulticlassAveragePrecision(num_classes=10, thresholds=5).to(hparams.device)
     auc = torchmetrics.classification.MulticlassAUROC(num_classes=10, thresholds=5).to(hparams.device)
-    # fpr95 = torchmetrics.FalsePositiveRate(num_thresholds=1000, pos_label=1, compute_on_step=False).to(hparams.device)
+    fpr95 = list()
     model = model.to(hparams.device)
     model.eval()
 
@@ -20,17 +20,25 @@ def test_cycle(model, hparams, val_loader):
             labels = labels.to(hparams.device)
 
             outputs = model(images)
+            outputs = torch.softmax(outputs, dim=1)
             acc.update(outputs, labels)
             ece.update(outputs, labels)
             aupr.update(outputs, labels)
             auc.update(outputs, labels)
-            # fpr95.update(outputs, labels)
+
+            # Calculate FPR95
+            # False positive rate at the 95th percentile
+            # fpr_outputs = torch.softmax(outputs, dim=1)
+            # print(outputs.shape)
+            # print(fpr_outputs.shape)
+            # print(outputs)
+            # print(fpr_outputs)
+            # raise
 
     total_acc = acc.compute()
     total_ece = ece.compute()
     total_aupr = aupr.compute()
     total_auc = auc.compute()
-    # total_fpr95 = fpr95.compute()
 
     return total_acc, total_ece, total_aupr, total_auc#, total_fpr95
 
@@ -55,6 +63,7 @@ def test_cycle_DE(models, hparams, val_loader):
             labels = labels.to(hparams.device)
 
             outputs = torch.stack([model(images) for model in models])
+            outputs = torch.softmax(outputs, dim=2)
             outputs = torch.mean(outputs, dim=0)
             acc.update(outputs, labels)
             ece.update(outputs, labels)
@@ -94,6 +103,7 @@ def test_cycle_PE(model, hparams, val_loader, num_models=4):
             # Output is of shape (batch_size, num_models * num_classes)
             # We need to reshape it to (batch_size, num_models, num_classes)
             outputs = outputs.reshape(images.shape[0], num_models, 10)
+            outputs = torch.softmax(outputs, dim=2)
             outputs = torch.mean(outputs, dim=1)
 
             acc.update(outputs, labels)
